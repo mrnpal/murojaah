5af0812065a87f44f2721f3b542c1fcf165b7140
mrnpal <mr.npal.dev@gmail.com>
2025-11-14 16:55:43 +0700

    third3aa

--- server/server.js
+++ server/server.js
@@ -151,6 +151,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🚀`);
 });
